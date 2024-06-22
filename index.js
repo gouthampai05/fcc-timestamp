92ba458f4c4a6606d771402dda6c4badce7c5201
gouthampai05 <gouthampai05@gmail.com>
2024-06-22 23:40:58 +0530

    base service works!

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
-
-
-// Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+app.get("/api/:inputdate", (req, res) => {
+  const inputDate = req.params.inputdate;
+
+  //assumes standard date format
+  let date = new Date(inputDate);
+
+  //will be NaN if input is unix time
+  if (isNaN(date.getTime())) {
+    date = new Date(inputDate * 1);
+  }
+
+  const unixTime = date.getTime();
+
+  const utcDay = date.toLocaleDateString("en-US", { weekday: "short" });
+  const utcDate = date.getDate();
+  const utcMonth = date
+    .toLocaleTimeString("en-US", { month: "short" })
+    .split(",")[0];
+  const utcYear = date.getFullYear();
+
+  const utcHours = date.getUTCHours();
+  const utcMinutes = date.getUTCMinutes();
+  const utcSeconds = date.getUTCSeconds();
+
+  res.json({
+    unix: unixTime,
+    utc: `${utcDay}, ${utcDate} ${utcMonth} ${utcYear} ${utcHours}:${utcMinutes}:${utcSeconds}`,
+  });
 });
