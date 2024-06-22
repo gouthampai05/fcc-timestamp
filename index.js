c299b3e24da37afee2e2d1dfc1b6262242ffe343
gouthampai05 <gouthampai05@gmail.com>
2024-06-22 23:52:49 +0530

    added fetching current datetime with an empty url param

--- index.js
+++ index.js
@@ -18,6 +18,28 @@ var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
+app.get("/api", (req, res) => {
+  let date = new Date();
+
+  const unixTime = date.getTime();
+
+  const utcDay = date.toLocaleDateString("en-US", { weekday: "short" });
+  const utcDate = date.getDate();
+  const utcMonth = date
+    .toLocaleTimeString("en-US", { month: "short" })
+    .split(",")[0];
+  const utcYear = date.getFullYear();
+
+  const utcHours = date.getUTCHours();
+  const utcMinutes = date.getUTCMinutes();
+  const utcSeconds = date.getUTCSeconds();
+
+  res.json({
+    unix: unixTime,
+    utc: `${utcDay}, ${utcDate} ${utcMonth} ${utcYear} ${utcHours}:${utcMinutes}:${utcSeconds} GMT`,
+  });
+});
+
 app.get("/api/:inputdate", (req, res) => {
   const inputDate = req.params.inputdate;
 
@@ -27,6 +49,10 @@ app.get("/api/:inputdate", (req, res) => {
   //will be NaN if input is unix time
   if (isNaN(date.getTime())) {
     date = new Date(inputDate * 1);
+  }
+
+  if (isNaN(date.getTime())) {
+    res.json({ error: "Invalid Date" });
   }
 
   const unixTime = date.getTime();
@@ -44,6 +70,6 @@ app.get("/api/:inputdate", (req, res) => {
 
   res.json({
     unix: unixTime,
-    utc: `${utcDay}, ${utcDate} ${utcMonth} ${utcYear} ${utcHours}:${utcMinutes}:${utcSeconds}`,
+    utc: `${utcDay}, ${utcDate} ${utcMonth} ${utcYear} 0${utcHours}:0${utcMinutes}:0${utcSeconds} GMT`,
   });
 });
